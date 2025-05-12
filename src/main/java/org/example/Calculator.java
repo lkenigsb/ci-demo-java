e890d4b53d4c4add4523194382b88fe772ac991a
Libby Elewitz <libbyelewitz@Libbys-MacBook-Pro.local>
2025-05-12 14:56:19 -0400

    added code + gitHub actions

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Calculator {
+
+    public double add(final double firstNum, final double secondNum)
+    {
+        return firstNum + secondNum;
+    }
+
+    public double subtract(final double firstNum, final double secondNum)
+    {
+        return firstNum - secondNum;
+    }
+}
---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest
+{
+    @Test
+    public void testAddNumbers()
+    {
+        // arrange
+        final Calculator calculator = new Calculator();
+
+        // act
+        final double sum = calculator.add(4, 3);
+
+        // assert
+        assertEquals(7, sum);
+    }
+
+    @Test
+    public void testSubtractNumbers()
+    {
+        // arrange
+        final Calculator calculator = new Calculator();
+
+        // act
+        final double subtracted = calculator.subtract(10, 3);
+
+        // assert
+        assertEquals(7, subtracted);
+    }
+}
